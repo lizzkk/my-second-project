25cc69ab9d19f6a07d02a52c7454508f70314c7d
lizzkk <osiryaevaelizaveta@gmail.com>
2024-11-19 14:25:48 +0900

    finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+public class pr1
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+    private static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+    public static void Main(string[] args)
+    {
+        int num1 = 8;
+        int num2 = 4;
+
+        int sum = Add(num1, num2);
+        Console.WriteLine($"Сумма {num1} и {num2} равна: {sum}");
+
+        bool isEven = IsEven(num1);
+        Console.WriteLine($"{num1} {(isEven ? "четное" : "нечетное")} число");
+    }  
+}
